3165db861b8b76790fa1ad91ad986d2352d2cd36
cureseven <shiningcureseven@gmail.com>
2020-06-15 07:03:37 +0000

    5章Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,4 +12,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select "title", full_title("Contact")
   end
+  
+    test "signup links" do
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+  
 end
