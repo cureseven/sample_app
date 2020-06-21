e37924ee19de4adb5852fe9b097f1a84b4bf4253
cureseven <shiningcureseven@gmail.com>
2020-06-21 10:33:26 +0000

    8章Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -8,4 +10,8 @@ Rails.application.routes.draw do
   
   resources :users
   post '/signup',  to: 'users#create'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
